5a8eec6324c45e843edf3d538fb1ffe6804f3406
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-31 16:03:57 +0100

    Aggiunta mappatura tra tabella project e technology

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,4 +15,7 @@ class Project extends Model
         return $this->belongsTo(Type::class);
     }
 
+    public function technlogies () {
+        return $this->belongsToMany(Technology::class);
+    }
 }
